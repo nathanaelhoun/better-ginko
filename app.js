8464ebdb3984957ffc780a933fe63c32c69fa364
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-12-23 18:54:02 +0100

    chore(style): indent go tabs and new app icon

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", async function (_e) {
-  document.onresize = () => {
-    document.body.style.height = window.innerHeight + "px";
-  };
+	document.onresize = () => {
+		document.body.style.height = window.innerHeight + "px";
+	};
 
-  document.onresize(null);
+	document.onresize(null);
 
-  const url =
-    "https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3";
+	const url =
+		"https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3";
 
-  const getTimes = async () => {
-    const response = await fetch(
-      "https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3"
-    );
+	const getTimes = async () => {
+		const response = await fetch(
+			"https://api.ginko.voyage/TR/getTempsLieu.do?idArret=CEPARGN2&nb=3"
+		);
 
-    const body = await response.json();
+		const body = await response.json();
 
-    /** @type {string[]} times */
-    const times = body.objets.listeTemps.map((o) => o.temps);
+		/** @type {string[]} times */
+		const times = body.objets.listeTemps.map((o) => o.temps);
 
-    if (times.length === 0) {
-      document.getElementById("next").innerText = "Plus de bus aujourd'hui...";
-      return;
-    }
+		if (times.length === 0) {
+			document.getElementById("next").innerText = "Plus de bus aujourd'hui...";
+			return;
+		}
 
-    document.getElementById("next").innerText = times.shift();
+		document.getElementById("next").innerText = times.shift();
 
-    document.getElementById("others").innerHTML = times
-      .map((time) => `<li>${time}</li>`)
-      .join("");
-  };
+		document.getElementById("others").innerHTML = times
+			.map((time) => `<li>${time}</li>`)
+			.join("");
+	};
 
-  setInterval(() => {
-    try {
-      getTimes();
-    } catch (e) {
-      document.getElementById("error").innerText = "T'as pas de réseau bolosse";
-    }
-  }, 500);
+	setInterval(() => {
+		try {
+			getTimes();
+		} catch (e) {
+			document.getElementById("error").innerText = "T'as pas de réseau bolosse";
+		}
+	}, 500);
 });
--- service-worker.js
+++ service-worker.js
@@ -1,72 +1,72 @@
 const cacheName = "GinkoBusCache";
 const contentToCache = [
-  "icons/icon-32.png",
-  "icons/icon-64.png",
-  "icons/icon-96.png",
-  "icons/icon-128.png",
-  "icons/icon-168.png",
-  "icons/icon-180.png",
-  "icons/icon-192.png",
-  "icons/icon-256.png",
-  "icons/icon-512.png",
-  "icons/maskable_icon.png",
-  "index.html",
-  "app.js",
-  "manifest.json",
-  "style.css",
+	"icons/icon-32.png",
+	"icons/icon-64.png",
+	"icons/icon-96.png",
+	"icons/icon-128.png",
+	"icons/icon-168.png",
+	"icons/icon-180.png",
+	"icons/icon-192.png",
+	"icons/icon-256.png",
+	"icons/icon-512.png",
+	"icons/maskable_icon.png",
+	"index.html",
+	"app.js",
+	"manifest.json",
+	"style.css",
 ];
 
 self.addEventListener("install", (e) => {
-  console.log("[SW] Install");
+	console.log("[SW] Install");
 
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName);
-      console.log("[SW] Caching all initial content");
-      await cache.addAll(contentToCache);
-    })()
-  );
+	e.waitUntil(
+		(async () => {
+			const cache = await caches.open(cacheName);
+			console.log("[SW] Caching all initial content");
+			await cache.addAll(contentToCache);
+		})()
+	);
 });
 
 const cachedFileExtensions = ["html", "css", "ico", "png", "json"];
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      /** @type {FetchEvent} e */
-      console.log(`[SW] Fetching resource: ${e.request.url}`);
-      const ext = e.request.url.split(".").pop();
+	e.respondWith(
+		(async () => {
+			/** @type {FetchEvent} e */
+			console.log(`[SW] Fetching resource: ${e.request.url}`);
+			const ext = e.request.url.split(".").pop();
 
-      if (cachedFileExtensions.includes(ext)) {
-        const res = await caches.match(e.request);
-        if (res) {
-          console.log("[SW] Found in cache !");
-          return res;
-        }
-      }
+			if (cachedFileExtensions.includes(ext)) {
+				const res = await caches.match(e.request);
+				if (res) {
+					console.log("[SW] Found in cache !");
+					return res;
+				}
+			}
 
-      console.log("[SW] Request server !");
+			console.log("[SW] Request server !");
 
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[SW] Caching new resource: ${e.request.url}`);
-      await cache.put(e.request, response.clone());
-      return response;
-    })()
-  );
+			const response = await fetch(e.request);
+			const cache = await caches.open(cacheName);
+			console.log(`[SW] Caching new resource: ${e.request.url}`);
+			await cache.put(e.request, response.clone());
+			return response;
+		})()
+	);
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key === cacheName) {
-            return;
-          }
-          return caches.delete(key);
-        })
-      );
-    })
-  );
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key === cacheName) {
+						return;
+					}
+					return caches.delete(key);
+				})
+			);
+		})
+	);
 });
